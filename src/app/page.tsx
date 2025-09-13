4706a54e4207885892e6422d6f2fb59ee6f3db54
anuragkumar <anuragpandey93345@gmail.com>
2025-09-13 18:30:12 +0530

    chore: initial project setup with Next.js and Convex integration

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Home() {
+  return <div className="m-10">
+    <SignInButton>
+      <Button>login</Button>
+    </SignInButton>
+  </div>;
+}
